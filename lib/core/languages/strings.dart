014d81c548cef5272c7a5d691803a13b655bc4fe
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-04-11 16:27:10 +0600

    complete

--- lib/core/languages/strings.dart
+++ lib/core/languages/strings.dart
@@ -163,14 +163,7 @@ class Strings {
   static const String healthCondition = "Health Conditions ";
   static const String termsConditions = "Terms & Conditions";
   static const String privacy = "Privacy & Policy";
-
-
-
-
-
-
-
-
+        
   static const String setYourGoal = "Set your goal";
   static const String youCanChooseSeveral = "You can choose several";
   static const String faqs = "FAQs";
